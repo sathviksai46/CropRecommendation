417dc5ff97df9203418b292403f7d7a058e08e0e
sathviksai46 <145713565+sathviksai46@users.noreply.github.com>
2024-09-23 21:51:44 +0530

    Create modules.py

---
+++ modules.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="CropRecommendation",
+    version="0.1",
+    install_requires=[
+        "streamlit",
+        "pandas",
+        "scikit-learn",
+    ],
+)
